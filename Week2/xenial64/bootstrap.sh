940785a24407b337577957ea710840c6e7253daf
keunglh <lkeung@hawk.iit.edu>
2017-01-20 15:54:08 -0600

    Altered bootstrap.sh to reflect the correct php install for Ubuntu 16.04 box

--- Week2/xenial64/bootstrap.sh
+++ Week2/xenial64/bootstrap.sh
@@ -2,7 +2,7 @@
 
 apt-get update
 apt-get install -y apache2
-apt-get install -y php5 libapache2-mod-php5 php5-mcrypt
+apt-get install -y php libapache2-mod-php php-mcrypt
 if ! [ -L /var/www ]; then
   rm -rf /var/www
   ln -fs /vagrant /var/www
